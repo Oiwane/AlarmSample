9deee5a76b52c31ec62add908a32735230b0d2ae
Oiwane Yuki <drhv8272@gmail.com>
2020-10-03 21:50:27 +0900

    アラームを追加する画面を仮作成した

--- app/src/main/java/io/github/oiwane/alarmsample/fragment/EditFragment.kt
+++ app/src/main/java/io/github/oiwane/alarmsample/fragment/EditFragment.kt
@@ -1,16 +1,16 @@
 package io.github.oiwane.alarmsample.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import io.github.oiwane.alarmsample.R
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * アラームを追加する画面
  */
 class EditFragment : Fragment() {
 
@@ -24,9 +24,14 @@ class EditFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val registerButton: Button = view.findViewById(R.id.registerButton)
+        val cancelButton: Button = view.findViewById(R.id.cancelButton)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        registerButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+        cancelButton.setOnClickListener {
+            findNavController().popBackStack()
         }
     }
 }
